8cbbc30eef8a04f4b77041aa39a67bda4cb7768d
Chetan Patil <cpatil@gmail.com>
2015-04-15 18:14:06 -0700

    drying

--- app/controllers/spree/admin/shippo_controller.rb
+++ app/controllers/spree/admin/shippo_controller.rb
@@ -1,22 +1,24 @@
 class Spree::Admin::ShippoController < Spree::Admin::BaseController
-    include SpreeShippoLabels
+  include SpreeShippoLabels
 
-    def show
-        @shippo_connect_endpoint = SpreeShippoLabels.get_auth_url
-        @store_url = SpreeShippoLabels::Config.instance.store_url
-        @store_name = SpreeShippoLabels::Config.instance.store_name
-        @partner_key = SpreeShippoLabels::Config.instance.partner_key
-        @api_token = SpreeShippoLabels.get_api_token
-        @register_automatically = SpreeShippoLabels::Config.instance.automatic_register_shippo_user
-        @user_usps_set = SpreeShippoLabels::Config.instance.store_usps_enabled
-    end
+  def show
+    cfg = SpreeShippoLabels::Config.instance
+    @store_url               = cfg.store_url
+    @store_name              = cfg.store_name
+    @partner_key             = cfg.partner_key
+    @register_automatically  = cfg.automatic_register_shippo_user
+    @user_usps_set           = cfg.store_usps_enabled
 
-    def view_order
-        redirect_to ( SpreeShippoLabels.get_orders_url(spree_current_user.email, params[:id]) )
-    end
+    @shippo_connect_endpoint = SpreeShippoLabels.get_auth_url
+    @api_token               = SpreeShippoLabels.get_api_token
+  end
 
-    def view_orders
-        redirect_to ( SpreeShippoLabels.get_orders_url(spree_current_user.email) )
-    end
+  def view_order
+    redirect_to (SpreeShippoLabels.get_orders_url(spree_current_user.email, params[:id]))
+  end
+
+  def view_orders
+    redirect_to (SpreeShippoLabels.get_orders_url(spree_current_user.email))
+  end
 
 end
